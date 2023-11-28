fcbd10ee70fa6fba7923d426adce9419f5f593fa
jookie <dovpeles@gmail.com>
2023-11-28 16:47:41 -0500

    Initial commit

---
+++ pub.sh
@@ -0,0 +1,9 @@
+# git remote add origin https://github.com/jookie/convex-replicate.git
+# git remote set-url origin https://github.com/jookie/convex-replicate.git
+# git branch -M main
+# git push -u origin main
+# export PATH=/usr/local/share/npm/bin:$PATH
+git add .
+git commit -m "Initial commit"
+git push 
+
